cfb2b38220802222a2c2183cd63121d2028a59f8
Nikhil Nair <nnair25@gmail.com>
2020-03-27 13:20:31 +0530

    Empty addition

--- src/utility.js
+++
@@ -1,8 +0,0 @@
-export const toSnakeCase = (str) => {
-    let newStr = str
-                .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
-                .map(x => x.toLowerCase())
-                .join('_');
-
-    return newStr;
-}
